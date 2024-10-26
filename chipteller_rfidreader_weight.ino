6fba09ffa7ccf47fd053ce979438b2464ebaff8a
shantell-labajo <123138462+shantell-labajo@users.noreply.github.com>
2024-10-25 18:23:07 -0700

    update with servos and more chip logic

--- chipteller_rfidreader_weight.ino
+++ chipteller_rfidreader_weight.ino
@@ -3,6 +3,7 @@
 #include <MFRC522.h>
 #include <Arduino.h>
 #include "HX711.h"
+#include <Servo.h>
 
 //for rfid
 #define SS_PIN 10
@@ -16,23 +17,45 @@
 #define stepPin2 34
 #define stepsPerRevolution 100
 
+//for servos
+Servo servoONE;
+Servo servoTWO;
+Servo servoTHREE;
+Servo servoFOUR;
+Servo servoFIVE;
+
+int servoDelay=250;
+int servoOpen=55;
+int servoClose=0;
+
 //initialize rfid module
 MFRC522 rfid(SS_PIN, RST_PIN);
 
 // HX711 circuit wiring
-const int LOADCELL_DOUT_PIN = 50;
-const int LOADCELL_SCK_PIN = 51;
-HX711 scale;
+// const int LOADCELL_DOUT_PIN = 50;
+// const int LOADCELL_SCK_PIN = 51;
+// HX711 scale;
 
-// "database" value to be read from $100 poker chip
-byte chip100block[] = {0xAA, 0xBB, 0xCC, 0xDD};
+// "database" value to be read from poker chip
+byte chip5block[] = {0x4E, 0xF8, 0x2A, 0xA6};
+byte chip50block[] = {0x01, 0x68, 0xE7, 0xFD};
+byte chip100block[] = {0x53, 0xCF, 0xA1, 0x4D};
+byte chip1000block[] = {0xCD, 0x93, 0x27, 0xE1};
+byte chip5000block[] = {0x4c, 0x8A, 0x05, 0x26};
 
-enum chipValue {FIVE, FIFTY, HUNDRED, THOUSAND, FIVETHOUSAND};
-chipValue chip; 
+// Array of pointers to each block
+byte* blocks[] = { chip5block, chip50block, chip100block, chip1000block, chip5000block };
+
+enum chipValue {FIVE, FIFTY, HUNDRED, THOUSAND, FIVETHOUSAND, OTHER};
+chipValue chip = OTHER; 
 
 // for counting chips
 int chipTotal = 0;
 bool doneCounting = false;
+int const timeDelay = 100;
+
+//for matching rfid values stored in poker chip
+bool pageMatch = false;
 
 void setup() {
 
@@ -40,6 +63,13 @@ void setup() {
   Serial.begin(57600);
   SPI.begin(); // init SPI bus
   rfid.PCD_Init(); // init MFRC522
+
+  // servo setup
+  servoONE.attach(40);   
+  servoTWO.attach(41);  
+  servoTHREE.attach(42); 
+  servoFOUR.attach(43); 
+  servoFIVE.attach(44);
 
   // motor pins
   pinMode(stepPin1, OUTPUT);
@@ -50,11 +80,11 @@ void setup() {
   delay(3000);
 
   // load cell weight initialize
-  Serial.println("Load cell weight setup.\n");
-  scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
-  scale.set_scale(113);
-  scale.tare();
-  Serial.println("Done.\n");
+  // Serial.println("Load cell weight setup.\n");
+  // scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
+  // scale.set_scale(113);
+  // scale.tare();
+  // Serial.println("Done.\n");
 
   // delay(3000);
   Serial.println("Setup success."); //for rfid reader
@@ -81,9 +111,6 @@ void loop() {
   byte buffer[18];
   byte size = sizeof(buffer);
 
-  //for matching rfid values stored in poker chip
-  bool pageMatch = true;
-
   // reading data in chip to match database
   Serial.print(F("Reading data from page ")); Serial.print(blockAddr);
   Serial.println(F("..."));
@@ -96,54 +123,56 @@ void loop() {
   dump_byte_array(buffer, 4); 
   Serial.println();
   
-  Serial.print(F("Database value: ")); dump_byte_array(chip100block, 4); Serial.println();
+  //Serial.print(F("Database value: ")); dump_byte_array(chip100block, 4); Serial.println();
 
   // Matching page of database to chip that was read.
-  for (byte i = 0; i < sizeof(chip100block); i++) {
-    if (buffer[i] != chip100block[i]) {
-      Serial.println("No match.");
-      pageMatch = false;
-      break;
-    }
-    else {
-      Serial.println("Match.");
-    }
-  }
+  // for (byte i = 0; i < sizeof(chip5block); i++) {
+  //   if (buffer[i] != chip5block[i]) {
+  //     //Serial.println("No match.");
+  //     pageMatch = false;
+  //     break;
+  //   }
+  //   else {
+  //     Serial.println("Match.");
+  //   }
+  // }
+  checkForMatch(buffer);
 
   // No match
-  if (!pageMatch) {
-    Serial.println("Invalid chip.");
-    delay(1000);
-  }
-  // match
-  else {
-    chip = HUNDRED;
-    digitalWrite(BL_LED, HIGH);
-    delay(1000);
-    digitalWrite(BL_LED, LOW);
-  }
+  // if (!pageMatch) {
+  //   Serial.println("Invalid chip.");
+  //   delay(500);
+  //   chip = OTHER;
+  // }
+  // // match
+  // else {
+  //   chip = HUNDRED;
+  //   // digitalWrite(BL_LED, HIGH);
+  //   // delay(1000);
+  //   // digitalWrite(BL_LED, LOW);
+  // }
 
   //move motor here
   spin_motor();
 
   //perform weight check
   bool is_valid_weight = true;
-  Serial.println("Begin weight check.");
-  scale.tare();
+  // Serial.println("Begin weight check.");
+  // scale.tare();
 
-  delay(1000);
-  Serial.print(scale.get_units(),1); Serial.println(" g");
+  // delay(1000);
+  // Serial.print(scale.get_units(),1); Serial.println(" g");
 
-  if (scale.get_units(10) > 4 && scale.get_units(10) < 6) {
-    Serial.println("\nValid chip weight.");
-    digitalWrite(LED_BUILTIN, HIGH);  
-    delay(1000);                     
-    digitalWrite(LED_BUILTIN, LOW);  
-    delay(1000);
-  }
-  else {
-   // is_valid_weight = false;
-  }
+  // if (scale.get_units(10) > 4 && scale.get_units(10) < 6) {
+  //   Serial.println("\nValid chip weight.");
+  //   digitalWrite(LED_BUILTIN, HIGH);  
+  //   delay(1000);                     
+  //   digitalWrite(LED_BUILTIN, LOW);  
+  //   delay(1000);
+  // }
+  // else {
+  //  // is_valid_weight = false;
+  // }
 
   //move motor here
   delay(100);
@@ -151,22 +180,65 @@ void loop() {
 
   //sorting logic w/ other motors here
   //if any boolean logic is fail; chips get spat out into reject pile
+  switch(chip) {
+        case FIVE:
+            setServos(false, true, false, false, false);
+            break;
+        case FIFTY:
+            setServos(false, false, true, false, false);
+            break;
+        case HUNDRED:
+            setServos(false, false, false, true, false);
+            break;
+        case THOUSAND:
+            setServos(false, false, false, false, true);
+            break;
+        case FIVETHOUSAND:
+            setServos(false, false, false, false, false);
+            break;
+        default:
+            setServos(true, false, false, false, false);
+            break;
+  }
 
   // chip total logic; check if all booleans are true
   if (pageMatch && is_valid_weight) {
-    chipTotal += 100;
+    switch(chip) {
+        case FIVE:
+            chipTotal += 5;
+            break;
+        case FIFTY:
+            chipTotal += 50;
+            break;
+        case HUNDRED:
+            chipTotal += 100;
+            break;
+        case THOUSAND:
+            chipTotal += 1000;
+            break;
+        case FIVETHOUSAND:
+            chipTotal += 5000;
+            break;
+        default:
+            chipTotal += 0;
+            break;
+    }
   }
 
-  delay(3000);
-
+  int i = 0;
   // check if more chips are being inserted
-  if (rfid.PICC_IsNewCardPresent()) {
-		return;
+  while ( i < timeDelay) {
+
+    if (rfid.PICC_IsNewCardPresent()) {
+		  return;
+    }
+
+    Serial.print(".");
+    i++;
 	}
-  else {
-    Serial.println("Total: "); Serial.println(chipTotal);
-    chipTotal = 0;
-  }
+
+  Serial.println("Total: "); Serial.println(chipTotal);
+  chipTotal = 0;
 
 }
 
@@ -195,3 +267,68 @@ void spin_motor() {
     delayMicroseconds(1000);
   }
 }
+
+// function to move the servo motors
+void setServos(bool one, bool two, bool three, bool four, bool five) {
+
+    servoONE.write(one ? servoOpen : servoClose);
+    servoTWO.write(two ? servoOpen : servoClose);
+    servoTHREE.write(three ? servoOpen : servoClose);
+    servoFOUR.write(four ? servoOpen : servoClose);
+    servoFIVE.write(five ? servoOpen : servoClose);
+    delay(servoDelay);
+
+}
+
+// Function to check if chip matches 
+void checkForMatch(byte* buffer) {
+  // Iterate over each block in the database
+  for (int j = 0; j < sizeof(blocks) / sizeof(blocks[0]); j++) {
+    if (matchPage(buffer, blocks[j])) {
+      Serial.println("Match found.");
+      pageMatch = true;
+
+      switch(j)  {
+          case 0:
+            chip = FIVE;
+            break;
+          case 1:
+            chip = FIFTY;
+            break;
+          case 2:
+            chip = HUNDRED;
+            break;
+          case 3:
+            chip = THOUSAND;
+            break;
+          case 4:
+            chip = FIVETHOUSAND;
+            break;
+          default:
+            break;
+      }
+
+      break; // Exit the loop once a match is found
+    }
+  }
+
+  if (!pageMatch) {
+    Serial.println("No match found.");
+    delay(100);
+    chip = OTHER;
+  }
+}
+
+// Function to compare two 8-byte arrays
+bool matchPage(byte* buffer, byte* block) {
+
+  Serial.print(F("Database value: ")); dump_byte_array(block, 4); Serial.println();
+  for (byte i = 0; i < 4; i++) {
+    //Serial.print(buffer[i], HEX); Serial.print(" "); Serial.println(block[i], HEX);
+    if (buffer[i] != block[i]) {
+      return false; // If any byte doesn't match, return false
+    }
+  }
+  return true; // All bytes matched
+}
+
